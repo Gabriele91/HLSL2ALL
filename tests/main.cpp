7286d2a954bd309c34d06da80645bff2c70c672d
Gabriele91 <dbgabri@gmail.com>
2018-07-22 13:46:27 +0200

    Update example

--- tests/main.cpp
+++ tests/main.cpp
@@ -10,28 +10,39 @@
 #include "HLSL2ALL/HLSL2ALL.h"
 
 
+static inline void replace_all(std::string& source, std::string const& find, std::string const& replace)
+{
+    for (std::string::size_type i = 0; (i = source.find(find, i)) != std::string::npos;)
+    {
+        source.replace(i, find.length(), replace);
+        i += replace.length();
+    }
+}
+
 int main(int argc, const char * argv[])
 {
     using namespace HLSL2ALL;
     std::string hlsl_filename("test.hlsl");
     std::string hlsl_source(
-    "#define IVec2 int2\n"
-    "#define IVec3 int3\n"
-    "#define IVec4 int4\n"
-    "#define IMat3 int3x3\n"
-    "#define IMat4 int4x4\n"
-    "#define Vec2 float2\n"
-    "#define Vec3 float3\n"
-    "#define Vec4 float4\n"
-    "#define Mat3 float3x3\n"
-    "#define Mat4 float4x4\n"
-    "#define DVec2 double2\n"
-    "#define DVec3 double3\n"
-    "#define DVec4 double4\n"
-    "#define DMat3 double3x3\n"
-    "#define DMat4 double4x4\n"
-    );
+R"HLSL(
+#define IVec2 int2
+#define IVec3 int3
+#define IVec4 int4
+#define IMat3 int3x3
+#define IMat4 int4x4
+#define Vec2 float2
+#define Vec3 float3
+#define Vec4 float4
+#define Mat3 float3x3
+#define Mat4 float4x4
+#define DVec2 double2
+#define DVec3 double3
+#define DVec4 double4
+#define DMat3 double3x3
+#define DMat4 double4x4
+)HLSL");
     hlsl_source += R"HLSL(
+    #line 45
     struct VertexShaderInput
     {
         Vec3 m_position : POSITION;
@@ -64,7 +75,10 @@ int main(int argc, const char * argv[])
     //global uniform
     float mask;
     Vec4 color;
-    
+
+    //texture
+	uniform sampler2D diffuse_texture;
+
     VertexShaderOutput vertex(VertexShaderInput input)
     {
         
@@ -81,15 +95,15 @@ int main(int argc, const char * argv[])
     
     Vec4 fragment(VertexShaderOutput input) : SV_TARGET
     {
-        if (input.m_color.a <= mask) discard;
-        return input.m_color * color;
+        Vec4 texture_color = tex2D(diffuse_texture, Vec2(1.0,1.0));
+        if (texture_color.a <= mask) discard;
+        return  color * Vec4(texture_color.rgb,1.0);
     }
     )HLSL";
-    
     //inputs
     std::string shader_target_name[ST_N_SHADER]
     {
-          "vertex"
+        "vertex"
         , "fragment"
         , "geometry"
         , "tass_control"
@@ -97,24 +111,32 @@ int main(int argc, const char * argv[])
         , "compute"
     };
     //save types
-    TypeSpirvShaderList shader_spirv_outputs;
-    ErrorSpirvShaderList shader_spirv_errors;
-    InfoSpirvShaderList shader_spirv_info
+    HLSL2ALL::TypeSpirvShaderList shader_spirv_outputs;
+    HLSL2ALL::ErrorSpirvShaderList shader_spirv_errors;
+    HLSL2ALL::InfoSpirvShaderList shader_spirv_info
     {
-        { ST_VERTEX_SHADER, shader_target_name[ST_VERTEX_SHADER] },
-        { ST_FRAGMENT_SHADER, shader_target_name[ST_FRAGMENT_SHADER] },
-        { ST_GEOMETRY_SHADER, shader_target_name[ST_GEOMETRY_SHADER] },
-        { ST_TASSELLATION_CONTROL_SHADER, shader_target_name[ST_TASSELLATION_CONTROL_SHADER] },
-        { ST_TASSELLATION_EVALUATION_SHADER, shader_target_name[ST_TASSELLATION_EVALUATION_SHADER] },
-        { ST_COMPUTE_SHADER, shader_target_name[ST_COMPUTE_SHADER] },
+        { HLSL2ALL::ST_VERTEX_SHADER, shader_target_name[HLSL2ALL::ST_VERTEX_SHADER] },
+        { HLSL2ALL::ST_TASSELLATION_CONTROL_SHADER, shader_target_name[HLSL2ALL::ST_TASSELLATION_CONTROL_SHADER] },
+        { HLSL2ALL::ST_TASSELLATION_EVALUATION_SHADER, shader_target_name[HLSL2ALL::ST_TASSELLATION_EVALUATION_SHADER] },
+        { HLSL2ALL::ST_GEOMETRY_SHADER, shader_target_name[HLSL2ALL::ST_GEOMETRY_SHADER] },
+        { HLSL2ALL::ST_FRAGMENT_SHADER, shader_target_name[HLSL2ALL::ST_FRAGMENT_SHADER] },
+        { HLSL2ALL::ST_COMPUTE_SHADER, shader_target_name[HLSL2ALL::ST_COMPUTE_SHADER] },
     };
+    //info target
+    TargetShaderInfo spirv_info;
+    spirv_info.m_client_version = 450;
+    spirv_info.m_desktop = true;
+    spirv_info.m_reverse_mul = true;
+    spirv_info.m_vulkan = false;
+    spirv_info.m_upgrade_texture_to_samples = false;
+    spirv_info.m_samplerarray_to_flat = false;
     //build
     if (!hlsl_to_spirv(  hlsl_source
                        , hlsl_filename
                        , shader_spirv_info
                        , shader_spirv_outputs
                        , shader_spirv_errors
-                       , TargetShaderInfo{ 450, false, true, true }
+                       , spirv_info
                        ))
     {
         for(auto&& error : shader_spirv_errors)
@@ -122,22 +144,45 @@ int main(int argc, const char * argv[])
             std::cout << error << std::endl;
         }
     }
-    
+    //...
+    HLSL2ALL::GLSLConfig glsl_config;
+    glsl_config.m_version = 410;
+    glsl_config.m_es = false;
+    glsl_config.m_vulkan_semantics = false;
+    glsl_config.m_rename_position_in_position0 = true;
+    glsl_config.m_fixup_clipspace = true;
+    glsl_config.m_flip_vert_y = false;
+    glsl_config.m_enable_420pack_extension = false;
+    glsl_config.m_combined_texture_samplers = false;
+    glsl_config.m_force_to_push_sample_uniform_as_texture = true;
+    HLSL2ALL::TextureSamplerList tex_samples;
     //to GLSL
     //spirv to glsl
     for (const TypeSpirvShader& ssoutput : shader_spirv_outputs)
     {
+        std::cout << "----------------- SHADER" << std::endl;
         //unpack
-        int type = std::get<0>(ssoutput);
-        SpirvShader shader_spirv_out = std::get<1>(ssoutput);
+        int type = ssoutput.m_type;
+        auto& shader_spirv_out = ssoutput.m_shader;
         //output shader
         std::string glsl_output;
-        //convert
-        GLSLConfig glsl_config;
-        if(type == ST_VERTEX_SHADER) glsl_config.m_rename_input_with_semantic = true;
-        spirv_to_glsl(shader_spirv_out, glsl_output, glsl_config);
+        //first
+        glsl_config.m_rename_input_with_semantic = type == HLSL2ALL::ST_VERTEX_SHADER;
+        //Apple bug
+        glsl_config.m_rename_input_with_locations = glsl_config.m_rename_output_with_locations;
+        glsl_config.m_input_prefix = glsl_config.m_output_prefix;
+        glsl_config.m_rename_output_with_locations = type != HLSL2ALL::ST_COMPUTE_SHADER;
+        glsl_config.m_output_prefix = std::string("__") + shader_target_name[type];
+        //compile
+        if (!HLSL2ALL::spirv_to_glsl(shader_spirv_out, glsl_output, tex_samples, shader_spirv_errors, glsl_config))
+        {
+            for(auto& err : shader_spirv_errors)
+            {
+                std::cout << err << std::endl;
+            }
+            return false;
+        }
         //cpp
-        std::cout << "----------------- SHADER" << std::endl;
         std::cout << glsl_output << std::endl;
     }
     
